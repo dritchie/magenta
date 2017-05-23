748014f8d470c72a442f7b9a86f2a11eb13b1b81
Maxime Voisin <maximev@stanford.edu>
2017-05-22 20:36:59 -0700

    Update __init__.py

--- musicgen/common/models/__init__.py
+++ musicgen/common/models/__init__.py
@@ -3,3 +3,4 @@ from common.models.sequenceGenerativeModel import SequenceGenerativeModel
 from common.models.rnnIndependent import RNNIndependent
 from common.models.rnnNade import RNNNade
 from common.models.rnnOrderlessNade import RNNOrderlessNade
+from common.models.rnnOrderlessNade import RNNOrderlessNadeConcat
